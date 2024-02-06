fe96f19c9208d0b16bf96abe2b35e09a3454e3a7
mariia1604 <103173096+mariia1604@users.noreply.github.com>
2024-02-06 15:28:53 +0500

    Update accept.jsx

--- client/src/accept.jsx
+++ client/src/accept.jsx
@@ -1,19 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { useSelector } from 'react-redux'
 
 function Accept(props) {
   const [status, setStatus] = useState('')
+
+  const token = useSelector((state) => state.auth.token)
 
   async function add(status) {
     const data = {
       'status': 'Принята',
       id: props.id
     }
-    await fetch("http://localhost:3000/accept/", {
+    await fetch("http://localhost:3000/edit_status/", {
       method: 'POST',
       mode: 'cors',
       headers: {
         "Content-Type": "application/json",
+        'Authorization': `Bearer ${token}`
       },
       body: JSON.stringify(data)
     });
@@ -23,14 +27,10 @@ function Accept(props) {
 
   return (
     <>
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        console.log("asjdsadjiasdj")
-        add(status)
-
-      }}>
+      <form>
         <div>
-          <input type="submit" id='accept' value="Принять" />
+          <input onClick={(e) => {e.preventDefault()
+          add()}} type="submit" id='accept' value="Принять" />
         </div>
       </form>
 
